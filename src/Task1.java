a593b934a776e0ab7d8a0940b002f02ccf79b6f9
temkame <temkame@yandex.ru>
2022-03-18 13:32:45 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,10 @@
+/*
+Дата рождения
+
+Вывести на экран дату своего рождения в виде: MAY 1 2012
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("MAY 1 2012");
+    }
+}
