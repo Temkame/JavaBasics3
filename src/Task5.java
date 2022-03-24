fc571e55b6c31625424ef3fcb5770af8fb8d21f7
temkame <temkame@yandex.ru>
2022-03-24 23:51:22 +0300

    Initial commit

---
+++ src/Task5.java
@@ -0,0 +1,16 @@
+/*
+Мама мыла раму
+
+Вывести на экран все возможные комбинации слов «Мама», «Мыла», «Раму».
+Подсказка: их 6 штук. Каждую комбинацию вывести с новой строки. Слова не разделять.
+ */
+public class Task5 {
+    public static void main(String[] args) {
+        System.out.println("МылаРамуМама");
+        System.out.println("РамуМамаМыла");
+        System.out.println("МамаМылаРаму");
+        System.out.println("МылаМамаРаму");
+        System.out.println("РамуМылаМама");
+        System.out.println("МамаРамуМыла");
+    }
+}
