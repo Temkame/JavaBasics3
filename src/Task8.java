3c5a26189aed885988b20086fc2f97e6fea85b72
temkame <temkame@yandex.ru>
2022-03-25 00:19:46 +0300

    Initial commit

---
+++ src/Task8.java
@@ -0,0 +1,13 @@
+/*
+Экранирование символов
+
+Вывести на экран следующий текст - две строки:
+It's Windows path: "C:\Program Files\Java\jdk1.7.0\bin"
+It's Java string: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"
+ */
+public class Task8 {
+    public static void main(String[] args) {
+        System.out.println("It's Windows path: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"");
+        System.out.println("It's Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\\\"");
+    }
+}
